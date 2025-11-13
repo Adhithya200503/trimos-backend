dba77e3e1487cd76e521fc7d5470539bf3755bd4
Adhithya200503 <adhithya.s.200503@gmail.com>
2025-11-13 22:14:22 +0530

    added route for google signin

--- controllers/authController.js
+++ controllers/authController.js
@@ -97,6 +97,7 @@ export const googleLogin = async (req, res) => {
             httpOnly: true,
             secure: true,
             sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
         return res.status(200).json({ message: "Login success", userData: user });
